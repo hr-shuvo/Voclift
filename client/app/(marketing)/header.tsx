e0fbf4f9e964f5934386c2003ffcecb20dfab4ca
hr-shuvo <h.r.shuvo9@gmail.com>
2024-11-13 14:47:26 +0600

    change name

--- client/app/(marketing)/header.tsx
+++ client/app/(marketing)/header.tsx
@@ -8,8 +8,7 @@ export const Header = () =>{
             <div className='lg:max-w-screen-lg mx-auto flex items-center justify-between h-full'>
                 <div className='pt-8 pl-4 pb-7 flex items-center gap-x-3'>
                     <Image src='/mascot.svg' height={40} width={40} alt='mascot'/>
-                    <h1 className='text-2xl font-extrabold text-green-500 tracking-wide'>bLingo</h1>
-
+                    <h1 className='text-2xl font-extrabold text-green-500 tracking-wide'>vocLift</h1>
                 </div>
 
                 {/* <Loader className='h-5 w-5'/> */}
