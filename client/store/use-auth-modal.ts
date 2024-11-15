1606e512ed31bbc89f1f59f97b65c1c9a5d395b5
hr-shuvo <h.r.shuvo9@gmail.com>
2024-11-15 07:24:54 +0600

    update modal

--- client/store/use-auth-modal.ts
+++ client/store/use-auth-modal.ts
@@ -1,18 +1,28 @@
 import {create} from "zustand";
 
 
-type AuthModalState = {
+type LoginModalState = {
     isOpen:boolean;
     openLogin:() =>void;
     close:() =>void;
 }
 
-export const useLoginModel = create<AuthModalState>((set) =>({
+export const useLoginModel = create<LoginModalState>((set) =>({
     isOpen:false,
     openLogin:() => set({isOpen:true}),
     close:() => set({isOpen:false}),
 }))
-export const useRegisterModel = create<AuthModalState>((set) =>({
+
+
+
+
+
+type RegisterModalState = {
+    isOpen:boolean;
+    openRegister:() =>void;
+    close:() =>void;
+}
+export const useRegisterModel = create<RegisterModalState>((set) =>({
     isOpen:false,
     openRegister:() => set({isOpen:true}),
     close:() => set({isOpen:false}),
